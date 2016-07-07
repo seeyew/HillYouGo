43ab51a26dac8c74a6d8745a25bce2fb3dd7f592
seeyew <seeyew@gmail.com>
2016-07-07 01:15:13 -0700

    Use OKHTTP interceptor to add api-key

--- app/src/main/java/com/seeyewmo/hillyougo/service/NYTService.java
+++ app/src/main/java/com/seeyewmo/hillyougo/service/NYTService.java
@@ -10,9 +10,10 @@ import rx.Observable;
  * Created by seeyew on 7/6/16.
  */
 public interface NYTService {
+    final String API_KEY_PARAM = "api-key";
     final String KEY = "8672bf1fd49d4a608fe6dec6f080ef79";
     final String SERVICE_ENDPOINT = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/";
 
-    @GET("{section}/{time-period}.json?api-key="+KEY)
+    @GET("{section}/{time-period}.json")
     Observable<NYTWrapper> getArticles(@Path("section") String section, @Path("time-period") int period);
 }
--- app/src/main/java/com/seeyewmo/hillyougo/service/ServiceFactory.java
+++ app/src/main/java/com/seeyewmo/hillyougo/service/ServiceFactory.java
@@ -3,12 +3,19 @@ package com.seeyewmo.hillyougo.service;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class ServiceFactory {
-
+    //TODO: Could be singleton
     /**
      * Creates a retrofit service from an arbitrary class (clazz)
      * @param clazz Java interface of the retrofit service
@@ -31,12 +38,27 @@ public class ServiceFactory {
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
+                .client(getOkHttpClientWithApiKey())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(JacksonConverterFactory.create(objectMapper))
                 .build();
         T service = retrofit.create(clazz);
 
         return service;
+    }
+
+    private static OkHttpClient getOkHttpClientWithApiKey() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request request = chain.request();
+                        HttpUrl url = request.url().newBuilder().addQueryParameter(NYTService.API_KEY_PARAM, NYTService.KEY).build();
+                        request = request.newBuilder().url(url).build();
+                        return chain.proceed(request);
+                    }
+                }).build();
+        return client;
     }
 
     /*
